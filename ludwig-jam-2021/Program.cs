c558316007ebfc75d91760a5f5cea45417bd8787
Popcorn05 <flynn@rewill.net>
2021-10-17 22:08:46 +1100

    Added Project

---
+++ ludwig-jam-2021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ludwig_jam_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
