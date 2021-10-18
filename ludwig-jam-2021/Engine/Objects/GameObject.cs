ac0458670af030ba79d6d015a17d7a148495a50c
Popcorn05 <43976660+Popcorn05@users.noreply.github.com>
2021-10-18 13:16:26 +1100

    Added Game Object Class and Resources Folder

---
+++ ludwig-jam-2021/Engine/Objects/GameObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ludwig_jam_2021.Engine.Objects
+{
+    //Abstract class because shouldn't be instanced
+    abstract class GameObject
+    {
+        //Gameworld position
+        private float x;
+        private float y;
+
+        //Render function, to be overridden in extensions
+        public abstract bool Render();
+    }
+}
